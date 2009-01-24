7999fbdd1491479c4038be8bd7757e37db1d8623
Alex Gaynor <alex.gaynor@gmail.com>
2009-01-24 11:18:05 -0500

    added a setup.py file

---
+++ setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+setup(
+    name='django-ajax-validation',
+    version='.5a',
+    description='Provides support for doing validation using Ajax(currently with jQuery) using your existing Django forms.',
+    long_description=open('docs/index.txt').read(),
+    author='Alex Gaynor',
+    author_email='alex.gaynor@gmail.com',
+    url='http://github.com/alex/django-ajax-validation/tree/master',
+    packages=[
+        'ajax_validation',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ]
+)
