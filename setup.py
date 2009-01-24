54c25f5faf9c6f65240a7c4f5af20a71dbbc568d
Alex Gaynor <alex.gaynor@gmail.com>
2009-01-24 11:34:14 -0500

    switched version, fixed name error

--- setup.py
+++ setup.py
@@ -2,13 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-ajax-validation',
-    version='.5a',
+    version='0.1.0',
     description='Provides support for doing validation using Ajax(currently with jQuery) using your existing Django forms.',
-    long_description=open('docs/index.txt').read(),
     author='Alex Gaynor',
     author_email='alex.gaynor@gmail.com',
     url='http://github.com/alex/django-ajax-validation/tree/master',
-    packages=find_pacakges(),
+    packages=find_packages(),
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Environment :: Web Environment',
