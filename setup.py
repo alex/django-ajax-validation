fc15fc3ed1d1fabe9af5e12d58538a44f6724d77
Alex Gaynor <alex.gaynor@gmail.com>
2009-01-29 22:48:26 -0500

    bumped dev version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-ajax-validation',
-    version='0.1.1',
+    version='0.1.2',
     description='Provides support for doing validation using Ajax(currently with jQuery) using your existing Django forms.',
     author='Alex Gaynor',
     author_email='alex.gaynor@gmail.com',
