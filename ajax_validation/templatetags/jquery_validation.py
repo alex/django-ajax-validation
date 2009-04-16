d08441c33e8155d6a50d734bf666f8b32c3e3e0e
Alex Gaynor <alex.gaynor@gmail.com>
2009-04-16 18:53:20 -0400

    load the JS script lazily

--- ajax_validation/templatetags/jquery_validation.py
+++ ajax_validation/templatetags/jquery_validation.py
@@ -6,8 +6,11 @@ import ajax_validation
 
 register = template.Library()
 
-VALIDATION_SCRIPT = open(os.path.join(os.path.dirname(ajax_validation.__file__), 'media', 'ajax_validation', 'js', 'jquery-ajax-validation.js')).read()
+VALIDATION_SCRIPT = None
 
 def include_validation():
+    global VALIDATION_SCRIPT
+    if VALIDATION_SCRIPT is None:
+        VALIDATION_SCRIPT = open(os.path.join(os.path.dirname(ajax_validation.__file__), 'media', 'ajax_validation', 'js', 'jquery-ajax-validation.js')).read()
     return '''<script type="text/javascript">%s</script>''' % VALIDATION_SCRIPT
 register.simple_tag(include_validation)
